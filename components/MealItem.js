8a7cb81189c0b7e208c3e8e33688770af91e8019
Albert Vuong <albert.vuong16@gmail.com>
2023-02-08 22:29:33 -0700

    Created Meal Details Screen

--- components/MealItem.js
+++ components/MealItem.js
@@ -1,13 +1,24 @@
 import { View, Text, Pressable, Image, StyleSheet } from "react-native";
 import { Platform } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import MealDetails from "./MealDetails";
 
-function MealItem ({ title, imageUrl, duration, complexity, affordability }) {
+function MealItem ({ id, title, imageUrl, duration, complexity, affordability }) {
+  const navigation = useNavigation();
+
+  function selectMealItemHandler() {
+    navigation.navigate('MealDetail', {
+      mealId: id
+    })
+  }
+
   return (
     <View style={styles.mealItem}>
       <Pressable 
         android_ripple={{color : '#ccc'}}
         style={({ pressed }) => 
-          (pressed ? styles.buttonPressed : null)}   
+          (pressed ? styles.buttonPressed : null)}
+        onPress={selectMealItemHandler}
       >
         <View>
           <View>
@@ -17,11 +28,7 @@ function MealItem ({ title, imageUrl, duration, complexity, affordability }) {
             />
             <Text style={styles.title}>{title}</Text>
           </View>
-          <View style={styles.details}>
-            <Text style={styles.detailItems}>{duration}m</Text>
-            <Text style={styles.detailItems}>{complexity.toUpperCase()}</Text>
-            <Text style={styles.detailItems}>{affordability.toUpperCase()}</Text>
-          </View>
+          <MealDetails duration={duration} affordability={affordability} complexity={complexity} />
         </View>
       </Pressable>
     </View>
@@ -59,14 +66,4 @@ const styles = StyleSheet.create({
     fontSize: 18,
     margin: 8
   },
-  details: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
-    padding: 8,
-  },
-  detailItems: {
-    marginHorizontal: 4,
-    fontSize: 12
-  }
 })
